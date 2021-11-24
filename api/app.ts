11a0cdd74c94e366a161e2f3bf14f8d7cd7cf385
Kevin Chan <kevinchan984@gmail.com>
2021-11-24 22:32:05 +0800

    added passport

--- api/app.ts
+++ api/app.ts
@@ -2,24 +2,32 @@ import express from 'express'
 const app = express()
 const port = 3001
 
-app.use(express.json)
-app.use(express.urlencoded)
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 app.get('/hello', (req, res) => {
     res.send('Hello World!')
 })
 
-app.post('sortnum', (req, res) => {
-    const {numArr} = req.body
+app.post('/sortnum', (req, res) => {
+    const { numArr } = req.body
     console.log(numArr);
-    
-    if (!Array.isArray(numArr) || !numArr.every(item=>isFinite(item))) {
-        res.status(400).json({error:"Invalid Input"})
+
+    if (!Array.isArray(numArr) || !numArr.every(item => isFinite(item))) {
+        res.status(400).json({ error: "Invalid Input" })
         return
     }
-    res.json(numArr.sort())
+
+    const json = numArr.map(number => parseFloat(number))
+    console.log(json);
+
+    res.json(json.sort((a, b) => a - b))
 
 })
+
+// app.post('/login', (req,res) => {
+//     const {login,password} = req.body
+// })
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
